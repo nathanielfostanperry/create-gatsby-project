5030e23f55608aaea1859df9455f24830144767a
Nathaniel Fostan <nathanielfostan@gmail.com>
2021-12-15 23:51:50 -0600

    update tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
-  content: [],
+  content: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   theme: {
     extend: {},
   },
